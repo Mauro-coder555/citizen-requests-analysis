253fd754b4976ddafd2cdaeb9fcf411a693c7c64
Mauro <maupereyra96@gmail.com>
2024-10-30 16:08:01 -0300

    limpieza de comentario

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# Conexión a la base de datos (esto puede ir en una función si prefieres)
+# Conexión a la base de datos
 config = {
     'user': 'user',
     'password': 'password',
